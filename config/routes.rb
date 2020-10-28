fb133dd943c047e41c52d02f07b0246e8520e21b
yumeyuniko <daigocafe@msn.com>
2020-10-28 18:39:29 +0900

    Finish ch03

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # root 'application#hello'
+  
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  
+  get 'static_pages/about'
+
 end
